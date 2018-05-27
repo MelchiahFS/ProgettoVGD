4850a1da8db441386f71edf151585d80e413113c
MelchiahFS <melchiah91@gmail.com>
2018-05-27 23:20:25 +0200

    Aggiunta playerCam e aggiornato i controlli del player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    public float speed;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    
     void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 50.0f;
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * 50.0f;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(x, y);
 
-        rb2d.AddForce(movement);
+        rb2d.velocity = movement * speed;
 
     }
+    
 }
