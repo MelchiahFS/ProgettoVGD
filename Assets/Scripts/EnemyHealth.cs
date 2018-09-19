aded1d778a2ee10a7f8481e691d3bd4e1ba7ba3b
MelchiahFS <melchiah91@gmail.com>
2018-09-19 17:37:03 +0200

    Aggiunto danno da contatto al player e durata invincibilità tra danni

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int damage = 10;
+    private int startingHealth = 50;
+    private int currentHealth;
+    private GameObject player;
+    private PlayerHealth playerHealth;
+	
+        
+    // Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "playerHitbox")
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    private int startingHealth = 100;
+    private int currentHealth;
+    private float timer = 0;
+    private float invincibilityTime = 3f; //periodo di invulnerabilità dopo aver ricevuto danno
+    private PlayerController pc;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        pc = GetComponent<PlayerController>();
+        currentHealth = startingHealth;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+	}
+
+    //i nemici possono danneggiare nuovamente il player solo dopo un certo periodo
+    public void TakeDamage(int amount)
+    {
+        if (timer > invincibilityTime)
+        {
+            timer = 0;
+            currentHealth -= amount; 
+            if (currentHealth <= 0)
+                PlayerDeath();
+        }
+        
+    }
+
+    private void PlayerDeath()
+    {
+        pc.enabled = false;
+    }
+}
