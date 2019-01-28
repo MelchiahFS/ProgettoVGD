6e7d1cbd8507561c160942c6de3b6e8930065b8d
MelchiahFS <melchiah91@gmail.com>
2019-01-28 16:05:56 +0100

    Corretto bug che impediva il caricamento di una nuova scena dalla schermata di gameOver

--- Assets/Scripts/AsyncLoader.cs
+++ Assets/Scripts/AsyncLoader.cs
@@ -8,15 +8,21 @@ public class AsyncLoader : MonoBehaviour {
 
     public Text loadingText;
     private bool loadScene = false;
+	private AsyncOperation async;
+
+	void Start()
+	{
+		Time.timeScale = 1;
+		async = null;
+		loadScene = false;
+		loadingText.text = "Loading Level " + GameStats.stats.levelNumber;
+	}
     
     void Update()
     {
         
-        loadingText.text = "Loading Level " + GameStats.stats.levelNumber;
-
         //fa lampeggiare il messaggio di caricamento modificando continuamente il canale alfa tra 0 e 1
         loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
-		Debug.Log("fuori coroutine");
         if (!loadScene)
             StartCoroutine(LoadNewScene());
 
@@ -26,22 +32,19 @@ public class AsyncLoader : MonoBehaviour {
     // Coroutine che carica la nuova scena in modo asincrono
     IEnumerator LoadNewScene()
 	{
-		Debug.Log("Dentro coroutine");
 		loadScene = true;
-        // This line waits for 3 seconds before executing the next line in the coroutine.
-        // This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
-        yield return new WaitForSeconds(3);
-
-
+		// This line waits for 3 seconds before executing the next line in the coroutine.
+		// This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
+		yield return new WaitForSeconds(3);
+		
         //Restituisce un'operazione asincrona che contiene informazioni sul progresso del caricamento
-        AsyncOperation async = SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+        async = SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
 
         // finché la nuova scena non è stata caricata non faccio nulla
         while (!async.isDone)
         {
             yield return null;
         }
-		loadScene = false;
 
 		yield break;
     }
