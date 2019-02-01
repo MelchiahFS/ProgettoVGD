33fe0cf6dd0ea5091fd4257928b968078b2726ff
MelchiahFS <melchiah91@gmail.com>
2019-02-01 18:46:35 +0100

    Aggiunta schermata dei comandi al menu principale

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -8,9 +8,12 @@ public class MenuManager : MonoBehaviour
 {
 
     public GameObject button;
+	public GameObject commands, mainMenu, inputHint;
     public AudioSource sound;
     public AudioClip move, select, music;
     public float time = 0.5f;
+	private bool menu = true, comm = false;
+	int i = 0;
 
     public void Start()
     {
@@ -22,24 +25,54 @@ public class MenuManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
-        {
-            sound.PlayOneShot(move, 0.5f);
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            sound.PlayOneShot(select, 0.5f);
-        }
+		//if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+		//{
+		//    sound.PlayOneShot(move, 0.5f);
+		//}
+		//else if (Input.GetKeyDown(KeyCode.Return))
+		//{
+		//    sound.PlayOneShot(select, 0.5f);
+		//}
 
-        if (EventSystem.current.currentSelectedGameObject == null)
-        {
-            EventSystem.current.SetSelectedGameObject(button);
-        }
-        else
-        {
-            button = EventSystem.current.currentSelectedGameObject;
-        }
-    }
+		//if (EventSystem.current.currentSelectedGameObject == null)
+		//{
+		//    EventSystem.current.SetSelectedGameObject(button);
+		//}
+		//else
+		//{
+		//    button = EventSystem.current.currentSelectedGameObject;
+		//}
+		if (menu)
+		{
+			if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+			{
+				sound.PlayOneShot(move, 0.5f);
+			}
+			else if (Input.GetKeyDown(KeyCode.Return))
+			{
+				sound.PlayOneShot(select, 0.5f);
+			}
+
+			if (EventSystem.current.currentSelectedGameObject == null)
+			{
+				EventSystem.current.SetSelectedGameObject(button);
+			}
+			else
+			{
+				button = EventSystem.current.currentSelectedGameObject;
+			}
+		}
+		else if (comm)
+		{
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				DeactivateCommandWindow();
+				sound.PlayOneShot(select, 0.5f);
+			}
+		}
+
+
+	}
 
 
     public void NewGameBtn(string newGameLevel)
@@ -65,4 +98,27 @@ public class MenuManager : MonoBehaviour
 		SceneManager.LoadScene("LoadingScreen");
 		yield break;
     }
+
+	public void ActivateCommandWindow()
+	{
+		button = EventSystem.current.currentSelectedGameObject;
+		commands.SetActive(true);
+		inputHint.SetActive(true);
+		comm = true;
+		mainMenu.SetActive(false);
+		menu = false;
+		sound.PlayOneShot(select, 0.5f);
+	}
+
+	public void DeactivateCommandWindow()
+	{
+		commands.SetActive(false);
+		inputHint.SetActive(false);
+		comm = false;
+		mainMenu.SetActive(true);
+		menu = true;
+		sound.PlayOneShot(select, 0.5f);
+		EventSystem.current.SetSelectedGameObject(null);
+		EventSystem.current.SetSelectedGameObject(button);
+	}
 }
